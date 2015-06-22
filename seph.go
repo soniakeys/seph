e8e89f4b39cbd555b688d49d3db8429fe51798d9
Sonia Keys <soniakeys@gmail.com>
2015-06-22 09:41:09 -0400

    wip

--- seph.go
+++ seph.go
@@ -8,17 +8,19 @@ import (
 	"io/ioutil"
 	"log"
 	"math"
-	"os"
+	"strings"
 	"time"
 
 	"github.com/soniakeys/meeus/base"
-	"github.com/soniakeys/meeus/elliptic"
 	"github.com/soniakeys/meeus/julian"
-	"github.com/soniakeys/meeus/kepler"
 	pp "github.com/soniakeys/meeus/planetposition"
-	"github.com/soniakeys/meeus/solarxyz"
+
+	"github.com/soniakeys/elliptic"
 	"github.com/soniakeys/mpcformat"
+	"github.com/soniakeys/observation"
 	"github.com/soniakeys/sexagesimal"
+
+	"github.com/naoina/toml"
 )
 
 // part of all the data in the full orbit record.
@@ -30,101 +32,52 @@ type part struct {
 	H                   float64 `val:"defNaN"`
 }
 
-// astrometricJ2000 cut and paste from meeus/elliptic, modified to return
-// additional values phase angle β and observer-object range Δ.
-//
-// AstrometricJ2000 is a utility function for computing astrometric coordinates.
-//
-// Argument f is a function that returns J2000 equatorial rectangular
-// coodinates of a body.
-//
-// Results are J2000 right ascention, declination, and elongation.
-func astrometricJ2000(f func(float64) (x, y, z, r float64), jde float64, e *pp.V87Planet) (α, δ, ψ, β, r, Δ float64) {
-	X, Y, Z := solarxyz.PositionJ2000(e, jde)
-	x, y, z, r := f(jde)
-	// (33.10) p. 229
-	ξ := X + x
-	η := Y + y
-	ζ := Z + z
-	Δ = math.Sqrt(ξ*ξ + η*η + ζ*ζ)
-	{
-		τ := base.LightTime(Δ)
-		x, y, z, r = f(jde - τ)
-		ξ = X + x
-		η = Y + y
-		ζ = Z + z
-		Δ = math.Sqrt(ξ*ξ + η*η + ζ*ζ)
-	}
-	α = math.Atan2(η, ξ)
-	if α < 0 {
-		α += 2 * math.Pi
-	}
-	δ = math.Asin(ζ / Δ)
-	R := math.Sqrt(X*X + Y*Y + Z*Z)
-	ψ = math.Acos((ξ*X + η*Y + ζ*Z) / R / Δ)
-	β = math.Acos((ξ*x + η*y + ζ*z) / r / Δ)
-	return
-}
-
-// position cut and paste from meeus/elliptic, modified to return additional
-// values.
-//
-// Position returns observed equatorial coordinates of a body with Keplerian elements.
-//
-// Argument e must be a valid V87Planet object for Earth.
-//
-// Results are right ascension and declination α and δ, and elongation ψ,
-// all in radians.  In addition, returns phase angle β, the sun-object distance
-// r in AU and the earth-object distance Δ in AU.
-func posFunc(k *elliptic.Elements, e *pp.V87Planet) func(jde float64) (α, δ, ψ, β, r, Δ float64) {
-	// (33.6) p. 227
-	n := base.K / k.Axis / math.Sqrt(k.Axis)
-	const sε = base.SOblJ2000
-	const cε = base.COblJ2000
-	sΩ, cΩ := math.Sincos(k.Node)
-	si, ci := math.Sincos(k.Inc)
-	// (33.7) p. 228
-	F := cΩ
-	G := sΩ * cε
-	H := sΩ * sε
-	P := -sΩ * ci
-	Q := cΩ*ci*cε - si*sε
-	R := cΩ*ci*sε + si*cε
-	// (33.8) p. 229
-	A := math.Atan2(F, P)
-	B := math.Atan2(G, Q)
-	C := math.Atan2(H, R)
-	a := math.Hypot(F, P)
-	b := math.Hypot(G, Q)
-	c := math.Hypot(H, R)
-
-	f := func(jde float64) (x, y, z, r float64) {
-		M := n * (jde - k.TimeP)
-		E, err := kepler.Kepler2b(k.Ecc, M, 15)
-		if err != nil {
-			E = kepler.Kepler3(k.Ecc, M)
-		}
-		ν := kepler.True(E, k.Ecc)
-		r = kepler.Radius(E, k.Ecc, k.Axis)
-		// (33.9) p. 229
-		x = r * a * math.Sin(A+k.ArgP+ν)
-		y = r * b * math.Sin(B+k.ArgP+ν)
-		z = r * c * math.Sin(C+k.ArgP+ν)
-		return
-	}
-	return func(jde float64) (α, δ, ψ, β, r, Δ float64) {
-		return astrometricJ2000(f, jde, e)
-	}
+type job struct {
+	Desig      string
+	Start, End time.Time
+	Site       string
 }
 
 func main() {
-	des := []byte(os.Args[1])
+	// read job file
+	td, err := ioutil.ReadFile("seph.job")
+	if err != nil {
+		log.Fatal(err)
+	}
+	var j job
+	if err = toml.Unmarshal(td, &j); err != nil {
+		log.Fatal(err)
+	}
+	// show stopper validations
+	if j.Desig == "" {
+		log.Fatal("no desig in seph.job")
+	}
+	if j.Start.IsZero() {
+		log.Fatal("no start in seph.job")
+	}
+	var par *observation.ParallaxConst
+	if strings.TrimSpace(j.Site) == "" {
+		j.Site = "500"
+	} else if j.Site != "500" {
+		var p observation.ParallaxMap
+		// bleh, parsing entire file wastes time here.
+		p, err = mpcformat.ReadObscodeDatFile("obscode.dat")
+		if err != nil {
+			log.Fatal(err)
+		}
+		var ok bool
+		if par, ok = p[j.Site]; !ok {
+			log.Fatal("site ", j.Site, " unknown")
+		}
+	}
+	// find desig in MPCORB.DAT
 	bd, err := ioutil.ReadFile("MPCORB.DAT")
 	if err != nil {
 		log.Fatal(err)
 	}
 	lines := bytes.Split(bd, []byte{'\n'})
 	var orbline []byte
+	des := []byte(j.Desig)
 	for _, line := range lines {
 		if bytes.HasPrefix(line, des) {
 			orbline = line
@@ -132,7 +85,7 @@ func main() {
 		}
 	}
 	if len(orbline) == 0 {
-		log.Fatal(os.Args[1], " not found")
+		log.Fatal("desig ", j.Desig, " not found")
 	}
 	var s part
 	su, err := mpcformat.NewExportUnmarshaler(&s)
@@ -147,18 +100,7 @@ func main() {
 		log.Fatal(err)
 	}
 	ep := julian.CalendarGregorianToJD(y, m, d)
-	t1, err := time.Parse("2006-01-02T15:04:05Z", os.Args[2])
-	if err != nil {
-		log.Fatal(err)
-	}
-	t2 := t1
 	// var inc time.Duration
-	if len(os.Args) > 3 {
-		t2, err = time.Parse("2006-01-02T15:04:05Z", os.Args[3])
-		if err != nil {
-			log.Fatal(err)
-		}
-	}
 	var el elliptic.Elements
 	el.Axis = s.A
 	el.Ecc = s.E
@@ -170,36 +112,25 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println(os.Args[1], "Epoch", y, m, d)
+	fmt.Println(j.Desig, "Epoch", y, m, d)
 	fmt.Println("\n           Time      RA          Dec        V     Elongation")
-	f := posFunc(&el, e)
-	α, δ, ψ, β, r, Δ := f(julian.TimeToJD(t1))
+	o := elliptic.NewOrbit(&el)
+	α, δ, ψ, β, r, Δ := o.PositionFromEarth(julian.TimeToJD(j.Start), e)
 	vs := ""
-	if v := vmag(s.H, s.G, β, r, Δ); v >= 6 {
+	if v := elliptic.Vmag(s.H, s.G, β, r, Δ); v >= 6 {
 		vs = fmt.Sprintf("%4.1f", v)
 	}
 	printLine := func(t time.Time) {
 		fmt.Printf("%s  %2v  %2v  %4s  %v\n", t.Format("2006-01-02 15:04:05"),
 			sexa.NewFmtRA(α), sexa.NewFmtAngle(δ), vs, sexa.NewFmtAngle(ψ))
 	}
-	printLine(t1)
-
-	α, δ, ψ, β, r, Δ = f(julian.TimeToJD(t2))
+	printLine(j.Start)
+	if j.End.IsZero() {
+		return
+	}
+	α, δ, ψ, β, r, Δ = o.PositionFromEarth(julian.TimeToJD(j.End), e)
 	if vs > "" {
-		vs = fmt.Sprintf("%4.1f", vmag(s.H, s.G, β, r, Δ))
+		vs = fmt.Sprintf("%4.1f", elliptic.Vmag(s.H, s.G, β, r, Δ))
 	}
-	printLine(t2)
-}
-
-func vmag(H, G, β, r, Δ float64) float64 {
-	if math.IsNaN(H) {
-		return H
-	}
-	if math.IsNaN(G) {
-		G = .15
-	}
-	tanβ2 := math.Tan(β / 2)
-	Φ1 := math.Exp(-3.33 * math.Pow(tanβ2, .63))
-	Φ2 := math.Exp(-1.87 * math.Pow(tanβ2, 1.22))
-	return H + 5*math.Log10(r*Δ) - 2.5*math.Log10((1-G)*Φ1+G*Φ2)
+	printLine(j.End)
 }
